d921e4219bd5cfe7c0ebd71948d04aa7ceac2520
Kaizoku <kaizoku04@yandex.ru>
2021-07-24 12:02:35 +0900

    tiles update

--- install/install.sql
+++ install/install.sql
@@ -749,6 +749,7 @@ CREATE TABLE `%PREFIX%planets` (
   `defensive_module_porcent` enum('0','1','2','3','4','5','6','7','8','9','10') NOT NULL DEFAULT '10',
   `military_module_porcent` enum('0','1','2','3','4','5','6','7','8','9','10') NOT NULL DEFAULT '10',
   `research_module_porcent` enum('0','1','2','3','4','5','6','7','8','9','10') NOT NULL DEFAULT '10',
+  `tiles_count` int(11) NOT NULL DEFAULT '14',
   PRIMARY KEY (`id`),
   KEY `id_luna` (`id_luna`),
   KEY `id_owner` (`id_owner`),
